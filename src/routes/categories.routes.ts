94304492d58eec8a9d069770e9f61838c2a6cd73
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-02 10:02:28 -0300

    categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 
 import { CategoryRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { PostgresCategoriesRepository } from '../modules/cars/repositories/PostgresCategoriesRepository';
 import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
+const specificationRoutes = Router()
+
 const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
@@ -22,6 +23,14 @@ categoriesRoutes.get("/", (request, response) => {
 
    return response.json(all);
 })
+
+specificationRoutes.post("/specification", (request, response) => {
+    const { name, description } = request.body;
+
+    
+})
+
+
 
 
 export { categoriesRoutes};
