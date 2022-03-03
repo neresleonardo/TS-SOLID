94bfe00968cbf290c580aa28c02ed5ff373560db
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-03 17:35:10 -0300

    Controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { CategoryRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const specificationRoutes = Router()
@@ -8,7 +9,7 @@ const specificationRoutes = Router()
 const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-   
+   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
